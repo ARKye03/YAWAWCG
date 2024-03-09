a332a462b27aeb17d942b4406b5bcedba39fa00d
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-09 14:54:08 -0500

    Add URP-HighFidelity-Renderer.asset and URP-HighFidelity.asset changes, and add YetAnotherScript.cs

--- Assets/scripts/YetAnotherScript.cs
+++ Assets/scripts/YetAnotherScript.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Start : MonoBehaviour
+public class YetAnotherScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
