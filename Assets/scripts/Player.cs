a42d4e24fa52c461fc898a53467899676c0e0b74
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-10 22:35:21 -0400

    Add Player class with deck and hand properties

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Cards;
+
+public class Player : MonoBehaviour
+{
+    public Deck deck;
+    public Hand hand;
+
+    // Implement methods to draw a card from the deck, play a card from the hand, etc.
+}
